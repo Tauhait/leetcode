c75f91b2157a0ef446160c32ac7f5dfff8d24b25
haifeng <chenhaifeng88888@gmail.com>
2017-10-02 21:18:20 -0700

    more probs

--- algorithms/java/Read N Characters Given Read4 II - Call multiple times.java
+++ algorithms/java/Read N Characters Given Read4 II - Call multiple times.java
@@ -28,3 +28,30 @@ public class Solution extends Reader4 {
         return k;
     }
 }
+/////////////////////
+/* The read4 API is defined in the parent class Reader4.
+      int read4(char[] buf); */
+
+public class Solution extends Reader4 {
+    /**
+     * @param buf Destination buffer
+     * @param n   Maximum number of characters to read
+     * @return    The number of characters read
+     */
+    char[] cbuf = new char[4];
+    int cbufIndex = 0;
+    int cbufSize = -1;
+    
+    public int read(char[] buf, int n) {
+        int index = 0;
+        while(index < n) {
+            while(cbufIndex < cbufSize && index < n) buf[index++] = cbuf[cbufIndex++];
+            if(cbufIndex >= cbufSize && index < n) {
+                if(cbufSize < 4 && cbufSize != -1) return index;
+                cbufSize = read4(cbuf);
+                cbufIndex = 0;
+            }
+        }
+        return index;
+    }
+}
--- algorithms/java/Read N Characters Given Read4.java
+++ algorithms/java/Read N Characters Given Read4.java
@@ -21,3 +21,25 @@ public class Solution extends Reader4 {
         return k;
     }
 }
+
+//////////
+/* The read4 API is defined in the parent class Reader4.
+      int read4(char[] buf); */
+
+public class Solution extends Reader4 {
+    /**
+     * @param buf Destination buffer
+     * @param n   Maximum number of characters to read
+     * @return    The number of characters read
+     */
+    public int read(char[] buf, int n) {
+        int index = 0;
+        char[] cbuf = new char[4];
+        while(index < n) {
+            int size = read4(cbuf);
+            for(int i = 0;i < size && index < n;++i) buf[index++] = cbuf[i];
+            if(size < 4) return index;
+        }
+        return index;
+    }
+}
